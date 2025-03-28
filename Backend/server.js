3c6f0b5dccebf3e7513bec109750c6b7811a21c4
Hritik <hritikjat260@gmail.com>
2025-03-28 17:45:15 +0530

    code-reviewer

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express');
+const aiRoutes = require('./src/routes/ai.routes')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+app.use('/ai', aiRoutes)
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
